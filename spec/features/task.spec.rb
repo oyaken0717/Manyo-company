c2f74060f8258dc721b9fa03bc34a42ff6fa5148
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-03-12 21:03:27 +0900

    task.spec.rbに記述

---
+++ spec/features/task.spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature "タスク管理機能", type: :feature do
+
+  scenario "タスク一覧のテスト" do
+
+  end
+
+  scenario "タスク作成のテスト" do
+
+  end
+
+  scenario "タスク詳細のテスト" do
+
+  end
+end

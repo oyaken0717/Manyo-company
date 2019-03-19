9437cafce6aba189558e9547ed2945e6a4c1cbed
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-03-19 18:04:04 +0900

    [add] タスクが優先順位の降順に並んでいるかのテスト実装

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -27,18 +27,18 @@ RSpec.feature "タスク管理機能", type: :feature do
     # 「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄に
     # タスクのタイトルと内容をそれぞれfill_in（入力）する
     # 2.ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
-    fill_in "task_title", with:"test_task_03"
+    fill_in "task_title", with:"test_task_01"
 
     # 3.ここに「タスク詳細」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
-    fill_in "task_content", with:"testtesttest3"
+    fill_in "task_content", with:"testtesttest1"
     # 「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）
     # 4.「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
     click_on "Create Task"
     # clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
     # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
     # 5.タスク詳細ページに、テストコードで作成したはずのデータ（記述）がhave_contentされているか（含まれているか）を確認（期待）するコードを書く
-    expect(page).to have_content 'test_task_03'
-    expect(page).to have_content 'testtesttest3'
+    expect(page).to have_content 'test_task_01'
+    expect(page).to have_content 'testtesttest1'
   end
 
   scenario "タスク詳細のテスト" do
@@ -47,8 +47,8 @@ RSpec.feature "タスク管理機能", type: :feature do
 
     page.first(".task-index__show-link").click
     # click_on "詳細"
-    expect(page).to have_content 'Factoryで作ったデフォルトのタイトル２'
-    expect(page).to have_content 'Factoryで作ったデフォルトのコンテント２'
+    expect(page).to have_content 'Factoryで作ったデフォルトのタイトル３'
+    expect(page).to have_content 'Factoryで作ったデフォルトのコンテント３'
   end
 
   scenario "タスクが作成日時の降順に並んでいるかのテスト" do
@@ -70,6 +70,5 @@ RSpec.feature "タスク管理機能", type: :feature do
     visit tasks_path
     click_on "優先順位でソートする"
     expect(Task.order("priority DESC").map(&:priority)).to eq ["高","中","低"]
-
   end
 end

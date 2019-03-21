2c290b57e2112a8424632729076236a0dc5f263e
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-03-21 18:18:09 +0900

    [add] 各ユーザが作った各タスクの詳細をadminの詳細画面で見られる

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,9 @@
 class Admin::UsersController < ApplicationController
   before_action :require_admin
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
-    @user = User.all
+    @users = User.select(:id, :name, :email, :admin)
   end
 
   def show; end
@@ -10,9 +12,7 @@ class Admin::UsersController < ApplicationController
     @user = User.new
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def create
     @user = User.new(user_params)
@@ -24,7 +24,6 @@ class Admin::UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」を更新しました。"
     else
@@ -33,7 +32,6 @@ class Admin::UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     redirect_to admin_user_url, notice:"ユーザー「#{@user.name}」を削除しました。"
   end
@@ -42,6 +40,10 @@ class Admin::UsersController < ApplicationController
 
   def require_admin
     redirect_to root_path unless current_user.admin?
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
   def user_params

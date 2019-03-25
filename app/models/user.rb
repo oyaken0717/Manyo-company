e4cd2027dc76f0e51007dc6930feec0518fcefe3
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-03-25 14:29:24 +0900

    [add] 管理ユーザが1人もいなくならないように削除の制御をする

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   before_validation { email.downcase! }
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password_digest, presence: true, length: { minimum: 6 }

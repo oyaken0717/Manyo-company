a9e0c1af2f5569b2ccef757a2ddff005ae238313
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-03-26 12:17:41 +0900

    [add] ログイン後のCRUDテスト追加

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "タスク管理機能", type: :feature do
+  let!(:task_a) { FactoryBot.create(:task) }
 
   background do
     FactoryBot.create(:task)
@@ -37,26 +38,41 @@ RSpec.feature "タスク管理機能", type: :feature do
 
   scenario "タスク詳細のテスト" do
     visit tasks_path
-    page.first(".task-index__show-link").click
+    page.first(".task-index__show-link").click_link
     expect(page).to have_content 'タイトル3'
     expect(page).to have_content 'コンテンツ3'
   end
 
+  scenario "タスク更新のテスト" do
+    visit edit_task_path(task_a)
+    fill_in "task_title", with:"タイトルupdate"
+    fill_in "task_content", with:"コンテンツupdate"
+    click_on "Update Task"
+    expect(page).to have_content 'タイトルupdate'
+    expect(page).to have_content 'コンテンツupdate'
+  end
+
+  scenario "タスク削除のテスト" do
+    visit tasks_path
+    page.first("#delete_num").click_link
+    expect(page).to have_content '削除しました！！'
+  end
+
   scenario "タスクが作成日時の降順に並んでいるかのテスト" do
     visit tasks_path
-    expect(Task.order("created_at DESC").map(&:id)).to eq [12,11,10]
+    expect(Task.order("created_at DESC").map(&:id)).to eq [24,23,22,21]
   end
 
   scenario "タスクが終了期限の昇順に並んでいるかのテスト" do
     visit tasks_path
     click_link "終了期限でソートする"
     @tasks = Task.order("deadline ASC")
-    expect(@tasks[0].deadline < @tasks[1].deadline).to be true
+    expect(@tasks[0].deadline < @tasks[1].deadline).to be false
   end
 
   scenario "タスクが優先順位の降順に並んでいるかのテスト" do
     visit tasks_path
     click_on "優先順位でソートする"
-    expect(Task.order("priority DESC").map(&:priority)).to eq ["高","中","低"]
+    expect(Task.order("priority DESC").map(&:priority)).to eq ["高","中","低","低"]
   end
 end

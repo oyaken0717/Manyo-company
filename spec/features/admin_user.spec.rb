c03243114a4bc3eb3ca2f10d131e31a2a7804309
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-03-26 17:41:36 +0900

    [add] ユーザー登録のテスト

--- spec/features/admin_user.spec.rb
+++ spec/features/admin_user.spec.rb
@@ -9,8 +9,19 @@ RSpec.feature "管理ユーザー機能", type: :feature do
     click_button 'ログイン'
   end
 
-  scenario "タスク一覧のテスト" do
+  scenario "ユーザーの一覧テスト" do
     visit admin_users_path
     expect(page).to have_content 'ad@ad.com'
+  end
+
+  scenario "ユーザー登録のテスト" do
+    visit new_admin_user_path
+    fill_in "user_name", with: "e@e.com"
+    fill_in "user_email", with: "e@e.com"
+    check 'user_admin'
+    fill_in "user_password", with: "e@e.com"
+    fill_in "user_password_confirmation", with: "e@e.com"
+    click_on "アカウント登録"
+    expect(page).to have_content 'e@e.com'
   end
 end

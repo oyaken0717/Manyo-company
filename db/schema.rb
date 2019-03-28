7d4a8f45605e1c0067d8064cbf26987a9c26d8f1
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-03-28 15:05:49 +0900

    [add] Labelテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_21_053001) do
+ActiveRecord::Schema.define(version: 2019_03_28_055710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "labels", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false

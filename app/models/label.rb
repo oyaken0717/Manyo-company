61eabdc3d7ef8bef00404695ccd4e992574b3bf6
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-03-28 15:36:12 +0900

    [add] アソシエーションでラベルの機能を作成

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,2 +1,5 @@
 class Label < ApplicationRecord
+  has_many :task_label, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
+  has_many :task_label_tasks, through: :task_label, source: :task
 end

ae0d763a243ba9b5b029318877b7c72fdacd366b
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-04-03 11:34:48 +0900

    [add] タグ作成機能実装

---
+++ app/controllers/labels_controller.rb
@@ -0,0 +1,25 @@
+class LabelsController < ApplicationController
+  def new
+    @label = Label.new
+  end
+
+  def create
+    @label = Label.new(label_params)
+    if @label.save
+      redirect_to admin_users_path, notice: "ラベルを作成しました。"
+    else
+      render "new", notice: "作成できませんでした。"
+    end
+  end
+
+  def destroy
+    @label = Label.find(params[:id])
+    @label.destroy
+    redirect_to admin_users_path, notice: "ラベルを削除しました。"
+  end
+  private
+
+  def label_params
+    params.require(:label).permit(:name)
+  end
+end
